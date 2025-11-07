61f2afdde751f40b2ce7f5b1cdbb2bc1f75956ee
o6b7 <alarkie2210@gmail.com>
2025-11-07 18:16:14 +0300

    Mock api

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
